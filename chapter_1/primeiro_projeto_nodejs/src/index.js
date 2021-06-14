94a77b84857fab5266f60e59034a4b494c82b619
Henrique Ritter <henrique-ritter@hotmail.com>
2021-06-14 18:11:54 -0300

    created customer Middleware

--- chapter_1/primeiro_projeto_nodejs/src/index.js
+++ chapter_1/primeiro_projeto_nodejs/src/index.js
@@ -8,6 +8,22 @@ app.use(express.json());
 // fake banco de dados
 const customers = [];
 
+//Middleware de validação de conta
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  // objeto(customer) que será repassado após o middleware
+  request.customer = customer;
+
+  return next();
+}
+
 /**
  * Dados de uma conta
  * cpf - string
@@ -38,16 +54,11 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
+// segunda forma de chamar um middlware(chama para todas as rotas abaixo)
+// app.use(verifyIfExistsAccountCPF);
 
-  //retorna cliente a partir do cpf
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if (!customer) {
-    return response.status(400).json({ error: "Customer not found!" });
-  }
-
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 

1cb7c2e6ff6d1448d5d930d7144bdc4ec84160ef
Henrique Ritter <henrique-ritter@hotmail.com>
2021-06-13 23:06:28 -0300

    Add customer/show customer statement

--- chapter_1/primeiro_projeto_nodejs/src/index.js
+++ chapter_1/primeiro_projeto_nodejs/src/index.js
@@ -18,16 +18,37 @@ const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  const customerAlreadyExists = customers.some(
+    //retorna verdade ou falso de acordo com a condicao
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
 
   customers.push({
     cpf,
     name,
-    id,
+    id: uuidv4(),
     statement: [],
   });
 
   return response.status(201).send();
+});
+
+app.get("/statement/:cpf", (request, response) => {
+  const { cpf } = request.params;
+
+  //retorna cliente a partir do cpf
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer doenst exists!" });
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);

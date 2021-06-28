590325c370cc0f77b3f6723cd2f2c7578ef6712f
Henrique Ritter <henrique-ritter@hotmail.com>
2021-06-28 17:58:45 -0300

    Created CreateCategoryService

---
+++ chapter_2/rentalx/src/services/CreateCategoryService.ts
@@ -0,0 +1,32 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+/**
+ * [x] - Definir tipo de retorno
+ * [x] - Alterar o retorno de erro
+ * [x] - Acessar o repositorio
+ */
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: CategoriesRepository) { }
+  // mesma coisa que :
+  // private categoriesRepository: CategoriesRepository;
+  // constructor(categoriesRepository: CategoriesRepository){
+  //   this.categoriesRepository=categoriesRepository;
+  // }
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+    if (categoryAlreadyExists) {
+      throw new Error("Category already exists");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };

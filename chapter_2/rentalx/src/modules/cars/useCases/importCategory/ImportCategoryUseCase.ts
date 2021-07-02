a724c6cd0d64f343cb08146f011cfcf9e8fc1385
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-01 21:08:48 -0300

    read stream file / add csv-parse lib

--- chapter_2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ chapter_2/rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,18 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    //
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = csvParse({});
+    stream.pipe(parseFile);
+
+    // recebe as linhas que o stream.pipe esta lendo
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 

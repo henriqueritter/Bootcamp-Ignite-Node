11024807c467e44f93290cd6a6e48229200b64e9
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-12 18:17:03 -0300

    Add bcrypt hash to user password

--- chapter_2/rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -8,15 +9,23 @@ class CreateUserUseCase {
   constructor(
     @inject("UsersRepository") private usersRepository: IUsersRepository
   ) { }
+
   async execute({
     name,
     password,
     email,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
-      password,
+      password: passwordHash,
       email,
       driver_license,
     });

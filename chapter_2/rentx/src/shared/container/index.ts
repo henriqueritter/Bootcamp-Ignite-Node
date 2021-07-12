039697a1eea53cdb3abce9e7d26dd46467ace8c4
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-12 17:14:06 -0300

    Created CreateUserUseCase and UsersRepo-Container

--- chapter_2/rentx/src/shared/container/index.ts
+++ chapter_2/rentx/src/shared/container/index.ts
@@ -1,7 +1,12 @@
 import { container } from "tsyringe";
 
+// users
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+// categories
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
+// specifications
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
 
@@ -14,4 +19,9 @@ container.registerSingleton<ICategoriesRepository>( // toda implementacao ICateg
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+);
+// container registerSingleton Interface
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository", // nome do container
+  UsersRepository // classe chamada
 );

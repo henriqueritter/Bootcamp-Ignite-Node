228bc5eefbd515d3fd3fccc612a960d887e69f35
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-12 19:07:30 -0300

    Created Auth controller

--- chapter_2/rentx/src/routes/authenticate.routes.ts
+++ chapter_2/rentx/src/routes/authenticate.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
 
+import { AuthenticateUserController } from "../modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+
 const authenticateRoutes = Router();
 
-authenticateRoutes.post("/sessions");
+const authenticateUserController = new AuthenticateUserController();
+
+authenticateRoutes.post("/sessions", authenticateUserController.handle);
 
 export { authenticateRoutes };
--- chapter_2/rentx/src/routes/index.ts
+++ chapter_2/rentx/src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
@@ -10,4 +11,5 @@ router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 
 router.use("/users", usersRoutes);
+router.use("/users/", authenticateRoutes);
 export { router };

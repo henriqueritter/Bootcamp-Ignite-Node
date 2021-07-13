b7e07955b0a132471274bee7c1a7b839df9e4dd2
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-12 22:04:40 -0300

    Created UpdateUserAvatarUseCase

--- chapter_2/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,9 +1,28 @@
-// Adicionar coluna avatar na tabela de users
+// [x] Adicionar coluna avatar na tabela de users
 // refatorar usuario com coluna avatar(entidade etc..)
 // Configuração upload multer
 // criar regra de negociodo upload
 // criar controller
+
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
 class UpdateUserAvatarUseCase {
-  async execute() { }
+  constructor(
+    @inject("UsersRepository") private usersRepository: IUsersRepository
+  ) { }
+  async execute({ avatar_file, user_id }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
 }
 export { UpdateUserAvatarUseCase };

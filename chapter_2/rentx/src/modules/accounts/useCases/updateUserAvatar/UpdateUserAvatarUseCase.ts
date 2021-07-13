43704a97e4d1367f8d26234b53864ac569656972
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-12 21:59:23 -0300

    Migration Add user avatar column to users table

---
+++ chapter_2/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,9 @@
+// Adicionar coluna avatar na tabela de users
+// refatorar usuario com coluna avatar(entidade etc..)
+// Configuração upload multer
+// criar regra de negociodo upload
+// criar controller
+class UpdateUserAvatarUseCase {
+  async execute() { }
+}
+export { UpdateUserAvatarUseCase };

7a06e30ffa15bc13e93e42cff2886e32c251af54
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-15 23:19:26 -0300

    created first example test

---
+++ chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,13 @@
+describe("Create category", () => {
+  it("2+2 should be 4", () => {
+    const sum = 2 + 2;
+    const result = 4;
+    expect(sum).toBe(result);
+  });
+
+  it("2+2 should not to be 5", () => {
+    const sum = 2 + 2;
+    const result = 5;
+    expect(sum).not.toBe(result);
+  });
+});

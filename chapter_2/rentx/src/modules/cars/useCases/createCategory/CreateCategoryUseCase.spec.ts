24fcafbd6c45ba0466d17b76f2d6662c6ceb6e94
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-17 11:33:35 -0300

    First test of categories repository

--- chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,13 +1,51 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
 describe("Create category", () => {
-  it("2+2 should be 4", () => {
-    const sum = 2 + 2;
-    const result = 4;
-    expect(sum).toBe(result);
+  let createCategoryUseCase: CreateCategoryUseCase;
+  let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(
+      categoriesRepositoryInMemory
+    );
   });
 
-  it("2+2 should not to be 5", () => {
-    const sum = 2 + 2;
-    const result = 5;
-    expect(sum).not.toBe(result);
+  it("should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "Category description Test",
+    };
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new category with name exists", async () => {
+    expect(async () => {
+      const category = {
+        name: "Category Test",
+        description: "Category description Test",
+      };
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+
+      await createCategoryUseCase.execute({
+        name: category.name,
+        description: category.description,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });

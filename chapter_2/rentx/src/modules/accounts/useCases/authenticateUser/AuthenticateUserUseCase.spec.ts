c3fa6c149eddde11d1b65f622707a7b0601a8400
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-19 20:57:18 -0300

    Created fake/in memory users repository

---
+++ chapter_2/rentx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,11 @@
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+
+describe("Authenticate User", () => {
+  beforeEach(() => { });
+
+  it("Should be able to authenticate user", async () => { });
+});

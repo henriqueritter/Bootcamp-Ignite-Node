a7b6655ee1b264c924d15d8f11b0e0989c047661
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-21 19:54:50 -0300

    add constructor to createCarUseCase

--- chapter_2/rentx/src/modules/cars/repositories/ICarsRepository.ts
+++ chapter_2/rentx/src/modules/cars/repositories/ICarsRepository.ts
@@ -1,2 +1,6 @@
-interface ICarsRepository { }
+import { ICreateCarDTO } from "../dtos/ICreatecarDTO";
+
+interface ICarsRepository {
+  create(data: ICreateCarDTO): Promise<void>;
+}
 export { ICarsRepository };

ced606bbae9f54074d5ad693ca691de3e58870b4
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-22 21:30:13 -0300

    Add TypeORM to Car Entity

--- chapter_2/rentx/src/modules/cars/infra/typeorm/entities/Car.ts
+++ chapter_2/rentx/src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,28 +1,50 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  JoinColumn,
+  ManyToOne,
+} from "typeorm";
 import { v4 as uuid } from "uuid";
 
+import { Category } from "./Category";
+
+@Entity("cars")
 class Car {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   name: string;
 
+  @Column()
   description: string;
 
+  @Column()
   daily_rate: number;
 
-  available: boolean;
+  @Column()
+  available = true; // atribui valor default como true e coloca a tipagem de boolean nessa propriedade
 
+  @Column()
   license_plate: string;
 
+  @Column()
   brand: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category: Category;
+
+  @Column() // assim teremos o id da categoria
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
   constructor() {
     if (!this.id) {
       this.id = uuid();
-      this.created_at = new Date();
-      this.available = true;
     }
   }
 }

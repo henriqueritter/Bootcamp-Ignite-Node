8e2215372554abc7eaeb320638c279227a27b2ec
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-22 22:59:53 -0300

    Refactored typeorm connection

--- chapter_2/rentx/src/shared/infra/typeorm/index.ts
+++ chapter_2/rentx/src/shared/infra/typeorm/index.ts
@@ -1,13 +1,24 @@
-import { createConnection, getConnectionOptions } from "typeorm";
+import { createConnection, getConnectionOptions, Connection } from "typeorm";
 
-interface IOptions {
-  host: string;
-}
+// Mudamos o metodo de conexao para rodar as seeds
+// interface IOptions {
+//   host: string;
+// }
+//
+// getConnectionOptions().then((options) => {
+//   const newOptions = options as IOptions;
+//   newOptions.host = "database"; // nome que esta no docker-composer.yml
+//   createConnection({
+//     ...options,
+//   });
+// });
 
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database"; // nome que esta no docker-composer.yml
-  createConnection({
-    ...options,
-  });
-});
+export default async (): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: "database",
+    })
+  );
+};

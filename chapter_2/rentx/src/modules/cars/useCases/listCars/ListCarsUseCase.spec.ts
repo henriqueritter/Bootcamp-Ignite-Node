5c182ee860e869edf36fa184241f0b078581b317
Henrique Ritter <henrique-ritter@hotmail.com>
2021-07-24 23:37:16 -0300

    Injected CarsRepositoryInMemory on listCars/Spec

--- chapter_2/rentx/src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ chapter_2/rentx/src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -1,9 +1,14 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+
 import { ListCarsUseCase } from "./ListCarsUseCase";
 
 let listCarsUseCase: ListCarsUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
 describe("List Cars", () => {
   beforeEach(() => {
-    listCarsUseCase = new ListCarsUseCase();
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    listCarsUseCase = new ListCarsUseCase(carsRepositoryInMemory);
   });
   it("should be able to list all available cars", async () => {
     await listCarsUseCase.execute();

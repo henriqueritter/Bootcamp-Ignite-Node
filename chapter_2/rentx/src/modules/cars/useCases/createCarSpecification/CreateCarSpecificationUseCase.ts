e35763c7573905af8f1b4a9c2c1dbfe8f2f65a8e
Henrique Ritter <henrique-ritter@hotmail.com>
2021-08-25 20:28:51 -0300

    Created appError test on CarSpecificationsUseCase

--- chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,4 +1,4 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { AppError } from "@shared/errors/AppError";
@@ -7,11 +7,13 @@ interface IRequest {
   car_id: string;
   specifications_id: string[];
 }
+
+@injectable()
 class CreateCarSpecificationUseCase {
   constructor(
-    // @inject("CarsRepository")
+    @inject("CarsRepository")
     private carsRepository: ICarsRepository
-  );
+  ) { }
   async execute({ car_id, specifications_id }: IRequest): Promise<void> {
     const carExists = await this.carsRepository.findById(car_id);
 

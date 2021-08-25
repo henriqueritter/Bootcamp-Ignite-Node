bf26ea363a548ef6c8540dd730cd28a9739f6394
Henrique Ritter <henrique-ritter@hotmail.com>
2021-08-25 20:31:45 -0300

    Created - Test add Specif to a car

--- chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.spec.ts
@@ -25,8 +25,20 @@ describe("Create Car Specification", () => {
   });
 
   it("should be able to create add a new specification to the car", async () => {
-    const car_id = "1234";
+    const car = await carsRepositoryInMemory.create({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC-1234",
+      fine_amount: 60,
+      brand: "Brand",
+      category_id: "category",
+    });
+
     const specifications_id = ["8989", "9090"];
-    await createCarSpecificationUseCase.execute({ car_id, specifications_id });
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specifications_id,
+    });
   });
 });

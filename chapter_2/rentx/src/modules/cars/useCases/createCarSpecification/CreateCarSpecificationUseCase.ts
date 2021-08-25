6e273a0a72b008b6c1fc955e514e738cf7c3ffcd
Henrique Ritter <henrique-ritter@hotmail.com>
2021-08-25 20:19:28 -0300

    Created findById method in ICarsReposit/inMemory

--- chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,8 +1,23 @@
+import { inject } from "tsyringe";
+
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { AppError } from "@shared/errors/AppError";
+
 interface IRequest {
   car_id: string;
   specifications_id: string[];
 }
 class CreateCarSpecificationUseCase {
-  async execute({ car_id, specifications_id }: IRequest): Promise<void> { }
+  constructor(
+    // @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  );
+  async execute({ car_id, specifications_id }: IRequest): Promise<void> {
+    const carExists = await this.carsRepository.findById(car_id);
+
+    if (!carExists) {
+      throw new AppError("Car doest not exists.");
+    }
+  }
 }
 export { CreateCarSpecificationUseCase };

15999dff21cb77f32ab25deeb1365492b4f626b4
Henrique Ritter <henrique-ritter@hotmail.com>
2021-09-13 21:24:47 -0300

    Fixed entity import on ISpecif/ICategory

--- chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IRequest {
@@ -12,7 +13,8 @@ interface IRequest {
 class CreateCarSpecificationUseCase {
   constructor(
     @inject("CarsRepository")
-    private carsRepository: ICarsRepository
+    private carsRepository: ICarsRepository,
+    private specificationsRepository: ISpecificationsRepository
   ) { }
   async execute({ car_id, specifications_id }: IRequest): Promise<void> {
     const carExists = await this.carsRepository.findById(car_id);
@@ -20,6 +22,11 @@ class CreateCarSpecificationUseCase {
     if (!carExists) {
       throw new AppError("Car doest not exists.");
     }
+
+    // array de obj coim as ids
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id
+    );
   }
 }
 export { CreateCarSpecificationUseCase };

cd5b00922146ea787928ad55e293e25633ca5175
Henrique Ritter <henrique-ritter@hotmail.com>
2022-01-08 11:34:46 -0300

    Created UploadCarImage UseCase

--- chapter_2/rentx/src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
+++ chapter_2/rentx/src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -1,1 +1,25 @@
-class UploadCarImageUseCase { }
+import { inject, injectable } from "tsyringe";
+
+import { ICarsImageRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject("CarsImagesRepository")
+    private carsImagesRepository: ICarsImageRepository
+  ) { }
+  // recebe array de paths das images
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    // pra cada imagem salva no banco
+    images_name.map(async (image_name) => {
+      await this.carsImagesRepository.create(car_id, image_name);
+    });
+  }
+}
+
+export { UploadCarImageUseCase };

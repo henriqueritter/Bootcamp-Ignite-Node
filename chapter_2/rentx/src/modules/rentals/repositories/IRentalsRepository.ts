dec9c6edb7ac0430c8be40db08e87ee646b3be09
Henrique Ritter <henrique-ritter@hotmail.com>
2022-01-09 14:14:06 -0300

    Created interface IRentalsRepository

---
+++ chapter_2/rentx/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,6 @@
+interface IRentalsRepository {
+  findOpenRentalByCar(car_id: string): Promise<Rental>;
+  findOpenRentalByUser(user_id: string): Promise<Rental>;
+}
+
+export { IRentalsRepository };
--- chapter_2/rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ chapter_2/rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,3 +1,5 @@
+import { AppError } from "@shared/errors/AppError";
+
 interface IRequest {
   user_id: string;
   car_id: string;
@@ -12,8 +14,21 @@ class CreateRentalUseCase {
     expected_return_date,
   }: IRequest): Promise<void> {
     // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo usuário.
-    const carAvailable =
+    const carUnavailable = await this.rentalsRepository.findOpenRentalByCar(
+      car_id
+    );
+
+    if (carUnavailable) {
+      throw new AppError("Car is unavailable.");
+    }
+
     // Não deve ser possível cadastrar um novo aluguel caso já exista um aberto para o mesmo carro.
+    const rentalOpenToUser = await this.rentalsRepository.findOpenRentalByUser(
+      user_id
+    );
+    if (rentalOpenToUser) {
+      throw new AppError("There is already a rental in progress for user.");
+    }
     // O Aluguel deve ter duração mínima de 24 horas.
   }
 }

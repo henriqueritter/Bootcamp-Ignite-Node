6e15a04ea8ac457e0eb12f30e5a5d5f79cd1a258
Henrique Ritter <henrique-ritter@hotmail.com>
2022-01-09 14:19:41 -0300

    Imported Rental Entity on IRentalsRepository

--- chapter_2/rentx/src/modules/rentals/repositories/IRentalsRepository.ts
+++ chapter_2/rentx/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,3 +1,5 @@
+import { Rental } from "../infra/typeorm/entities/Rental";
+
 interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;

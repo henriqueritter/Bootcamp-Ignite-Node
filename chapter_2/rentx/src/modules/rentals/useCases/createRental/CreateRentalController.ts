43526b7a1a67deed01ab3346169659e6c4bb1544
Henrique Ritter <henrique-ritter@hotmail.com>
2022-01-29 15:08:47 -0300

    Created Rental Route

--- chapter_2/rentx/src/modules/rentals/useCases/createRental/CreateRentalController.ts
+++ chapter_2/rentx/src/modules/rentals/useCases/createRental/CreateRentalController.ts
@@ -4,16 +4,18 @@ import { container } from "tsyringe";
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 class CreateRentalController {
-  async handle(request: Request, response: Response) {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { expected_return_date, car_id } = request.body;
     const { id } = request.user;
     const createRentalUseCase = container.resolve(CreateRentalUseCase);
 
-    await createRentalUseCase.execute({
+    const rental = await createRentalUseCase.execute({
       car_id,
       expected_return_date,
       user_id: id,
     });
+
+    return response.status(201).json(rental);
   }
 }
 

7151054724298725b21c75e81ab4222e1cca5002
Henrique Ritter <henrique-ritter@hotmail.com>
2022-01-29 16:30:19 -0300

    Renamed column expected_return_date in Migration

--- chapter_2/rentx/src/shared/infra/typeorm/migrations/1641656294132-CreateRentals.ts
+++ chapter_2/rentx/src/shared/infra/typeorm/migrations/1641656294132-CreateRentals.ts
@@ -29,7 +29,7 @@ export class CreateRentals1641656294132 implements MigrationInterface {
                         type: "timestamp",
                     },
                     {
-                        name: "expect_return_date",
+                        name: "expected_return_date",
                         type: "timestamp",
                     },
                     {

03881aefbc5e4385c26793c0ed92f1c53cd282ee
Henrique Ritter <henrique-ritter@hotmail.com>
2022-01-30 16:06:39 -0300

    created category controller spec file

--- chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -3,5 +3,7 @@ import request from "supertest";
 import { app } from "@shared/infra/http/app";
 
 describe("Create Category Controller", async () => {
-  await request(app).get("/cars/available").expect(200);
+  it("test", async () => {
+    await request(app).get("/cars/available").expect(200);
+  });
 });

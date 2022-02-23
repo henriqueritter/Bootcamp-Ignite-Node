0c711995d8c4c1f814162dd485a2acdabf5383ff
Henrique Ritter <henrique-ritter@hotmail.com>
2022-02-23 08:40:37 -0300

    Added admin seed query in controller category test

--- chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,8 +1,27 @@
+import { hash } from "bcrypt";
 import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuid } from "uuid";
 
 import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm";
 
+let connection: Connection;
 describe("Create Category Controller", async () => {
+  beforeEach(async () => {
+    connection = await createConnection();
+
+    const id = uuid();
+    const password = await hash("admin", 8);
+    // const date = new Date();
+
+    await connection.query(
+      `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+      values('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'XXXXXXXX')
+      `
+    );
+  });
+
   it("should be albe to create a new category", async () => {
     const response = await request(app).post("/categories").send({
       name: "Category Supertest",

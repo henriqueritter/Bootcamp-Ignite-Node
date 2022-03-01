7f9b84e5d62434e4b58010bba416942f67fac888
Henrique Ritter <henrique-ritter@hotmail.com>
2022-03-01 19:09:49 -0300

    add updateAvailable to createRentalUseCase

--- chapter_2/rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ chapter_2/rentx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -68,6 +68,9 @@ class CreateRentalUseCase {
       expected_return_date,
     });
 
+    //define o status da disponibilidade do carro como false 
+    await this.carsRepository.updateAvailable(car_id, false);
+
     return rental;
   }
 }

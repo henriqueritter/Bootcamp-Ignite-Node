9cad4fe09f43f04e256a37dbe8bab0d92101004c
henriqueritter <henrique-ritter@hotmail.com>
2022-03-16 21:03:40 -0300

    Implemented findById to rentalsRepository

--- chapter_2/rentx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ chapter_2/rentx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -10,14 +10,22 @@ class RentalsRepository implements IRentalsRepository {
   constructor() {
     this.repository = getRepository(Rental);
   }
+
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     const openByCar = await this.repository.findOne({ car_id });
     return openByCar;
   }
+
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
     const openByUser = await this.repository.findOne({ user_id });
     return openByUser;
   }
+
+  async findById(id: string): Promise<Rental> {
+    const rental = await this.repository.findOne({ id });
+    return rental;
+  }
+
   async create({
     car_id,
     expected_return_date,
--- chapter_2/rentx/src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ chapter_2/rentx/src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -17,6 +17,10 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
     );
   }
 
+  async findById(id: string): Promise<Rental> {
+    return this.rentals.find((rental) => rental.id === id);
+  }
+
   async create({
     car_id,
     user_id,

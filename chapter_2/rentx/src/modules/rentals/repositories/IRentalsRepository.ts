6d418aaa07c8687bb50225bb7d75fd85ce516bb3
henriqueritter <henrique-ritter@hotmail.com>
2022-03-17 22:54:29 -0300

    Created execute and handle from listRentalsByUser

--- chapter_2/rentx/src/modules/rentals/repositories/IRentalsRepository.ts
+++ chapter_2/rentx/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,6 +5,7 @@ interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
   findById(id: string): Promise<Rental>;
+  findByUserId(user_id: string): Promise<Rental[]>;
   create(data: ICreateRentalDTO): Promise<Rental>;
 }
 

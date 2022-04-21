6dd1e14f2da33d76608ec8f9847205c0928dd847
henriqueritter <henrique-ritter@hotmail.com>
2022-04-20 22:05:07 -0300

    [feat] add findByUserIdAndRefreshToken method to IUsersTokensRepository and implemented to repository

--- chapter_2/rentx/src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ chapter_2/rentx/src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -11,6 +11,7 @@ class UsersTokensRepository implements IUsersTokensRepository {
   constructor() {
     this.repository = getRepository(UserTokens);
   }
+
   async create({
     expires_date,
     refresh_token,
@@ -25,6 +26,18 @@ class UsersTokensRepository implements IUsersTokensRepository {
     await this.repository.save(userToken);
 
     return userToken;
+  }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string
+  ): Promise<UserTokens> {
+    const userTokens = await this.repository.findOne({
+      user_id,
+      refresh_token,
+    });
+
+    return userTokens;
   }
 }
 
--- chapter_2/rentx/src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -1,5 +1,37 @@
+import { verify } from "jsonwebtoken"; // verifica se o token é valido
+import { inject } from "tsyringe";
+
+import auth from "@config/auth";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IPayload {
+  sub: string;
+}
+
 class RefreshTokenUseCase {
-  async execute() { }
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository
+  ) {
+    /** */
+  }
+  async execute(token: string) {
+    // verifica se  o token de refresh é valido conforme a secret do arquivo auth.ts
+    const decode = verify(token, auth.secret_refresh_token) as IPayload;
+    // recupera somente o userid do sub
+    const user_id = decode.sub;
+    // procura pelo usuario no repositorio de tokens
+    const userTokens =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        user_id,
+        token
+      );
+    if (!userTokens) {
+      // se nao encontrar usuario na tabela de refresh token estoura um erro
+      throw new AppError("Refresh Token does not exists!");
+    }
+  }
 }
 
 export { RefreshTokenUseCase };

4a6d0aca6003eafff6e6383e615fd198ab813c9c
henriqueritter <henrique-ritter@hotmail.com>
2022-04-20 22:34:58 -0300

    [fix] add injectable to RefreshTokenUseCase.ts

--- chapter_2/rentx/src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { RefreshTokenUseCase } from "./RefreshTokenUseCase";
 
 class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
-    return response.send();
+    // podemos receber o token por qualquer uma das tres formas a seguir
+    const token =
+      request.body.token ||
+      request.headers["x-access-token"] ||
+      request.query.token;
+
+    const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
+
+    const refresh_token = await refreshTokenUseCase.execute(token);
+
+    return response.json(refresh_token);
   }
 }
 

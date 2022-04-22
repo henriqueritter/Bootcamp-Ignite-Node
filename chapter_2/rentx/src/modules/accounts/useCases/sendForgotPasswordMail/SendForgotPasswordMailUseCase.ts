56fcb7117c919b4f6de8c77bf848821eb63e94f9
henriqueritter <henrique-ritter@hotmail.com>
2022-04-22 11:31:14 -0300

    [feat] implemented sendForgotPasswordMailController

--- chapter_2/rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.ts
@@ -1,5 +1,20 @@
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
 class SendForgotPasswordMailUseCase {
-  async execute(email: string) { }
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+  async execute(email: string) {
+    const emailExists = await this.usersRepository.findByEmail(email);
+    if (!emailExists) {
+      throw new AppError("Email not found.");
+    }
+  }
 }
 
 export { SendForgotPasswordMailUseCase };

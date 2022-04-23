efe36a9c8ff5d8edc212053ca59e3b999f3414d6
henriqueritter <henrique-ritter@hotmail.com>
2022-04-23 15:32:24 -0300

    [tests] created first scenario of sendMail to check if useCase is calling the mailProvider

--- chapter_2/rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
 import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { MailProviderInMemory } from "@shared/container/providers/MailProvider/in-memory/MailProviderInMemory";
 
 import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
 
@@ -9,17 +10,34 @@ let sendForgotPasswordMailUseCase: SendForgotPasswordMailUseCase;
 let usersRepositoryInMemory: UsersRepositoryInMemory;
 let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
 let dateProvider: DayjsDateProvider;
+let mailProvider: MailProviderInMemory;
 
 describe("Send Forgot Mail", () => {
   beforeEach(() => {
     usersRepositoryInMemory = new UsersRepositoryInMemory();
     usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
     dateProvider = new DayjsDateProvider();
+    mailProvider = new MailProviderInMemory();
+
     sendForgotPasswordMailUseCase = new SendForgotPasswordMailUseCase(
       usersRepositoryInMemory,
       usersTokensRepositoryInMemory,
-      dateProvider
+      dateProvider,
+      mailProvider
     );
   });
-  it("should be able to send a forgot password mail to user", () => { });
+  it("should be able to send a forgot password mail to user", async () => {
+    const sendMailEvent = jest.spyOn(mailProvider, "sendMail");
+
+    await usersRepositoryInMemory.create({
+      driver_license: "123456",
+      email: "test@rentx.com",
+      name: "Foo",
+      password: "12345",
+    });
+
+    await sendForgotPasswordMailUseCase.execute("test@rentx.com");
+
+    expect(sendMailEvent).toHaveBeenCalled();
+  });
 });
---
+++ chapter_2/rentx/src/shared/container/providers/MailProvider/in-memory/MailProviderInMemory.ts
@@ -0,0 +1,22 @@
+import { ISendEmailDTO } from "../dtos/ISendEmailDTO";
+import { IMailProvider } from "../IMailProvider";
+
+class MailProviderInMemory implements IMailProvider {
+  private message: any[] = [];
+
+  async sendMail({
+    to,
+    subject,
+    variables,
+    path,
+  }: ISendEmailDTO): Promise<void> {
+    this.message.push({
+      to,
+      subject,
+      variables,
+      path,
+    });
+  }
+}
+
+export { MailProviderInMemory };

c6bf46c3da9ba72d7cb1095f1ba33f71a437cbe8
henriqueritter <henrique-ritter@hotmail.com>
2022-04-23 13:52:50 -0300

    [feat] created findByRefreshToken method in usersTokensRepository

--- chapter_2/rentx/src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/resetPasswordUser/ResetPasswordUserUseCase.ts
@@ -1,9 +1,28 @@
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { AppError } from "@shared/errors/AppError";
+
+interface IRequest {
+  token: string;
+  password: string;
+}
 @injectable()
 class ResetPasswordUserUseCase {
-  constructor() { }
-  async execute() { }
+  constructor(
+    @inject("UsersTokensRepository")
+    private usersTokensRepository: IUsersTokensRepository
+  ) { }
+
+  async execute({ password, token }: IRequest) {
+    const userToken = await this.usersTokensRepository.findByRefreshToken(
+      token
+    );
+
+    if (!userToken) {
+      throw new AppError("Token Invalid!");
+    }
+  }
 }
 
 export { ResetPasswordUserUseCase };

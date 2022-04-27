f2db3182046f577a8104b7aa9c213f8ddff34337
henriqueritter <henrique-ritter@hotmail.com>
2022-04-27 20:01:28 -0300

    [tests] changed integration tests to use the token instead of refresh_token in authentications

--- chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ chapter_2/rentx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -35,7 +35,7 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
@@ -44,7 +44,7 @@ describe("Create Category Controller", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer: ${refresh_token}`,
+        Authorization: `Bearer: ${token}`,
       });
 
     expect(response.status).toBe(201);
@@ -56,7 +56,7 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
@@ -65,7 +65,7 @@ describe("Create Category Controller", () => {
         description: "Category Supertest",
       })
       .set({
-        Authorization: `Bearer: ${refresh_token}`,
+        Authorization: `Bearer: ${token}`,
       });
 
     expect(response.status).toBe(400);

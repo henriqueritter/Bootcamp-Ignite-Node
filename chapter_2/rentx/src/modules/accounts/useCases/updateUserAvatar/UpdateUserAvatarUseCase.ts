80c97fd2ce200bbd98124d6074d5b0890036a607
henriqueritter <henrique-ritter@hotmail.com>
2022-04-27 23:11:16 -0300

    [feat] injected storageProvider to updateAvatarUseCase

--- chapter_2/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ chapter_2/rentx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -2,7 +2,9 @@ import { inject, injectable } from "tsyringe";
 
 // funcao para deletar o arquivo
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
-import { deleteFile } from "@utils/file";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
+
+// import { deleteFile } from "@utils/file";
 
 interface IRequest {
   user_id: string;
@@ -12,14 +14,23 @@ interface IRequest {
 @injectable()
 class UpdateUserAvatarUseCase {
   constructor(
-    @inject("UsersRepository") private usersRepository: IUsersRepository
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) { }
+
   async execute({ avatar_file, user_id }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
 
     if (user.avatar) {
-      await deleteFile(`./tmp/avatar/${user.avatar}`); // deleta o avatar antigo do usuario
+      // passamos a funcao de delete do proivder
+      await this.storageProvider.delete(user.avatar, "avatar");
     }
+
+    // passamos a funcao de save do provider
+    await this.storageProvider.save(avatar_file, "avatar");
 
     user.avatar = avatar_file;
 

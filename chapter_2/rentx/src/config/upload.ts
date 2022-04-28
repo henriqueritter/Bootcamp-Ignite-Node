31e10f35682b3fe3fbedaea165f1a0446f5b1886
henriqueritter <henrique-ritter@hotmail.com>
2022-04-27 22:49:18 -0300

    [feat] created implementation LocalStorageProvider / changed multer upload function  in config/upload

--- chapter_2/rentx/src/config/upload.ts
+++ chapter_2/rentx/src/config/upload.ts
@@ -2,19 +2,19 @@ import crypto from "crypto";
 import multer from "multer";
 import { resolve } from "path";
 
-export default {
-  // funcao export recebe o caminho
-  upload(folder: string) {
-    return {
-      storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", folder),
-        filename: (request, file, callback) => {
-          const fileHash = crypto.randomBytes(16).toString("hex"); // cria o hash para o nome do arquivo
-          const fileName = `${fileHash}-${file.originalname}`; // concatena o nome original do arquivo com o hash criado
+const tmpFolder = resolve(__dirname, "..", "..", "tmp");
 
-          return callback(null, fileName);
-        },
-      }),
-    };
-  },
+export default {
+  tmpFolder,
+  // funcao export recebe o caminho
+
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (request, file, callback) => {
+      const fileHash = crypto.randomBytes(16).toString("hex"); // cria o hash para o nome do arquivo
+      const fileName = `${fileHash}-${file.originalname}`; // concatena o nome original do arquivo com o hash criado
+
+      return callback(null, fileName);
+    },
+  }),
 };

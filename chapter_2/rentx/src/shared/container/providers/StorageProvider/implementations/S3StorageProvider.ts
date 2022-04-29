71c38ecf9b17921606909f0c485ab9203237a1f4
henriqueritter <henrique-ritter@hotmail.com>
2022-04-28 23:01:17 -0300

    [feat] implemented S3 putObject in S3StorageProvider

--- chapter_2/rentx/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ chapter_2/rentx/src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -1,4 +1,9 @@
 import { S3 } from "aws-sdk";
+import fs from "fs";
+import mime from "mime";
+import { resolve } from "path";
+
+import upload from "@config/upload";
 
 import { IStorageProvider } from "../IStorageProvider";
 
@@ -6,13 +11,40 @@ class S3StorageProvider implements IStorageProvider {
   private client: S3;
   constructor() {
     this.client = new S3({
-      region: "sa-east-1",
+      region: process.env.AWS_BUCKET_REGION,
     });
   }
-  save(file: string, folder: string): Promise<string> {
-    throw new Error("Method not implemented.");
+  async save(file: string, folder: string): Promise<string> {
+    // pegamos o arquivo que foi inserido na pasta TMP
+    const originalName = resolve(upload.tmpFolder, file);
+
+    const fileContent = await fs.promises.readFile(originalName);
+
+    // retorna o contentType
+    const ContentType = mime.getType(originalName);
+
+    await this.client
+      .putObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+        ACL: "public-read",
+        Body: fileContent,
+        ContentType,
+      })
+      .promise(); // para transformar esse put object em uma promise
+
+    // apos inserido removemos o arquivo do nosso tmp local
+    await fs.promises.unlink(originalName);
+
+    return file;
   }
-  delete(file: string, folder: string): Promise<void> {
-    throw new Error("Method not implemented.");
+  async delete(file: string, folder: string): Promise<void> {
+    // para deletar o arquivo
+    await this.client
+      .deleteObject({
+        Bucket: `${process.env.AWS_BUCKET}/${folder}`,
+        Key: file,
+      })
+      .promise();
   }
 }

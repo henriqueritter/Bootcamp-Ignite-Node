f029be439f538687ae097d80aa09614192cc29bd
henriqueritter <henrique-ritter@hotmail.com>
2022-04-30 12:46:02 -0300

    [feat] created UserMap and implemented to profileUserUseCase

--- chapter_2/rentx/src/modules/accounts/mappers/UserMap.ts
+++ chapter_2/rentx/src/modules/accounts/mappers/UserMap.ts
@@ -1,8 +1,23 @@
+import { IUserResponseDTO } from "../dtos/IUserResponseDTO";
 import { User } from "../infra/typeorm/entities/User";
 
 class UserMap {
   // serve para quando chamar o toDTO nao precisemos fazer um new UserMap e sim direto UserMap.toDTO
-  static toDTO({ id, email, name, avatar, driver_license }: User) { }
+  static toDTO({
+    id,
+    email,
+    name,
+    avatar,
+    driver_license,
+  }: User): IUserResponseDTO {
+    return {
+      id,
+      email,
+      name,
+      avatar,
+      driver_license,
+    };
+  }
 }
 
 export { UserMap };

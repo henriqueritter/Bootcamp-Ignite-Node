8b69f3895ef968ec30fbdb82971cf56300b844b2
henriqueritter <henrique-ritter@hotmail.com>
2022-04-30 12:13:48 -0300

    [feat] inject storageProvider to uploadCarImage useCase

--- chapter_2/rentx/src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ chapter_2/rentx/src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -11,13 +12,17 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImagesRepository: ICarsImagesRepository
+    private carsImagesRepository: ICarsImagesRepository,
+
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) { }
   // recebe array de paths das images
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     // pra cada imagem salva no banco
     images_name.map(async (image_name) => {
       await this.carsImagesRepository.create(car_id, image_name);
+      await this.storageProvider.save(image_name, "cars");
     });
   }
 }

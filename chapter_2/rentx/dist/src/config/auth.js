9d659d104aa71f209821c74e6c0b911e781d5986
henriqueritter <henrique-ritter@hotmail.com>
2022-05-02 21:45:02 -0300

    [build] fixed babel.config

--- chapter_2/rentx/dist/src/config/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secret_token: "9eb71ab7420eb452a22787ca4fab501b",
-    expires_in_token: "15m",
-    secret_refresh_token: "221cc96be62e87cb536a9c270a339337",
-    expires_in_refresh_token: "30d",
-    expires_refresh_token_days: 30,
-};

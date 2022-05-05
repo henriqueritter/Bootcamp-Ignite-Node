2efec5a63b5a4754547ac1059ac41e47bc779c4a
henriqueritter <henrique-ritter@hotmail.com>
2022-05-04 21:48:13 -0300

    [deploy] add cors to api

--- chapter_2/rentx/src/shared/infra/http/app.ts
+++ chapter_2/rentx/src/shared/infra/http/app.ts
@@ -10,6 +10,8 @@ import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
 
+import cors from "cors";
+
 import swaggerFile from "../../../swagger.json";
 import { router } from "./routes";
 
@@ -24,6 +26,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 // middleware de erro, erro sempre vem primeiro

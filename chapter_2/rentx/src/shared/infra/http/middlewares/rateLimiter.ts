f96ab2bc39fa58f0084cc64f837547d57584f2d6
henriqueritter <henrique-ritter@hotmail.com>
2022-05-05 22:42:32 -0300

    [feat] created rateLimiter middleware

---
+++ chapter_2/rentx/src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import redis from "redis";
+
+import { AppError } from "@shared/errors/AppError";
+
+const redisClient = redis.createClient({
+  legacyMode: true,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 10, // requests permitidas por
+  duration: 5, // 5 segundos
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError("Too many requests", 429);
+  }
+}

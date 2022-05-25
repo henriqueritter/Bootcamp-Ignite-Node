08a387fe0e15fbe94cf2568ceabe0a8f7fd96873
henriqueritter <henrique-ritter@hotmail.com>
2022-05-24 23:24:46 -0300

    Add resources users_certificate DynamoDB Table

--- chapter_6/certificateignite/serverless.ts
+++ chapter_6/certificateignite/serverless.ts
@@ -45,6 +45,34 @@ const serverlessConfiguration: AWS = {
       concurrency: 10,
     },
   },
+  //resources vamos criara estrutura de tabela(a mesma no dinamodb)
+  resources: {
+    Resources: {
+      dbCertificateUsers: { //nome da table
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users_certificate",
+          ProvisionedThroughput: { //controle de quota
+            ReadCapacityUnits: 5,//quantas reqs por leitura
+            WriteCapacityUnit: 5 //quantas reqs por escrita
+          },
+          //props da nossa table
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",  //nossa PK
+              AttributeType: "S"  //S para string 
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
